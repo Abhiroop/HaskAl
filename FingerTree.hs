6d1150b0611361d151d565fd5bdac83e03b48865
Abhiroop Sarkar <asiamgenius@gmail.com>
2017-02-12 11:53:54 +0530

    Refactor newtype

--- FingerTree.hs
+++ FingerTree.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
+
 module FingerTree where
 
 import Data.Monoid
 
 data Tree v a = Leaf v a | Branch v (Tree v a) (Tree v a)
 
-newtype Size a = Size {getSize :: Int}
+newtype Size = Size {getSize :: Int}
 
 -------------List with random acces----------------
 
@@ -15,13 +16,13 @@ toList :: Tree v a -> [a]
 toList (Leaf _ a)     = [a]
 toList (Branch _ x y) = toList x ++ toList y
 
--- leaf :: a -> Tree (Size a) a
+-- leaf :: a -> Tree Size a
 -- leaf a = Leaf (Size 1) a
 
--- branch :: Tree (Size a) a -> Tree (Size a) a -> Tree (Size a) a
+-- branch :: Tree Size a -> Tree Size a -> Tree Size a
 -- branch x y = Branch (Size (tag x + tag y)) x y
 
--- tag :: Tree (Size a) a -> Int
+-- tag :: Tree Size a -> Int
 -- tag (Leaf v _)     = getSize v
 -- tag (Branch v _ _) = getSize v
 
@@ -29,24 +30,24 @@ head' :: Tree v a -> a
 head' (Leaf _ a) = a
 head' (Branch _ x _) = head' x
 
-(!!!) :: Tag (Size a) => Tree (Size a) a-> Int -> a
+(!!!) :: Tag Size => Tree Size a-> Int -> a
 (Leaf _ a)     !!! 0 = a
 (Branch _ x y) !!! n
   | n < getSize (tag x) = x !!! n
   | otherwise           = y !!! (n - getSize (tag x)) 
 
-instance Monoid (Size a)  where
+instance Monoid Size  where
   mempty  = Size 0
   Size x `mappend` Size y = Size (x + y)
 
 -----------Priority Queue with O(log n) lookup
-newtype Priority a = Priority {getPriority :: Int}
+newtype Priority = Priority {getPriority :: Int}
 
--- tag' :: Tree (Priority a) a -> Int
+-- tag' :: Tree Priority a -> Int
 -- tag' (Leaf v _)     = getPriority v
 -- tag' (Branch v _ _) = getPriority v
 
-winner :: Tag (Priority a) => Tree (Priority a) a -> a
+winner :: Tag Priority => Tree Priority a -> a
 winner t = go t
    where
    go (Leaf _ a) = a
@@ -54,23 +55,20 @@ winner t = go t
      | getPriority (tag x) == getPriority (tag t) = go x -- winner on left
      | getPriority (tag y) == getPriority (tag t) = go y -- winner on right3
 
-
-instance Monoid (Priority a) where
+instance Monoid Priority where
   mempty  =  Priority (maxBound :: Int)
   Priority x `mappend` Priority y = Priority (min x y)
-
 
 class Tag t where
   tag :: Tree t a -> t
 
-instance Tag (Tree (Size a) a) where
+instance Tag (Tree Size a) where
   tag (Leaf v _)     = v
   tag (Branch v _ _) = v
 
-instance Tag (Tree (Priority a) a) where
+instance Tag (Tree Priority a) where
   tag (Leaf v _)     = v
   tag (Branch v _ _) = v
-
 
 branch :: (Tag v, Monoid v) => Tree v a -> Tree v a -> Tree v a
 branch x y = Branch (tag x <> tag y) x y
@@ -79,13 +77,13 @@ branch x y = Branch (tag x <> tag y) x y
 class Monoid v => Measured v a where
     measure :: a -> v
 
-leaf :: Measured v a => a -> Tree v a
-leaf a = Leaf (measure a) a
+leaf :: Measured v a => a -> a -> Tree v a
+leaf a priority = Leaf (measure priority) a
 
-instance Measured (Size a) Int where
+instance Measured Size Int where
     measure _ = Size 1            -- one element = size 1
 
-instance Measured (Priority a) Int where
+instance Measured Priority Int where
     measure a = Priority a   -- urgency of the element
 
 instance (Tag v, Measured v a) => Measured v (Tree v a) where
@@ -101,4 +99,5 @@ search p t
     go i p (Branch _ l r)
         | p (i <> measure l) = go i p l
         | otherwise          = go (i <> measure l) p r
+
 
